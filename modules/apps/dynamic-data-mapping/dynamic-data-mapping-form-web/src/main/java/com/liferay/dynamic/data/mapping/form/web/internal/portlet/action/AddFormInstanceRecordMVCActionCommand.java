d62eea3ffc8e0132a5d0cb5594d3451332b83a80
Ivica Čardić <ivica.cardic@gmail.com>
2024-01-15 10:31:45 +0100

    e-obrazac - Send confirmation email for guest users

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/AddFormInstanceRecordMVCActionCommand.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/AddFormInstanceRecordMVCActionCommand.java
@@ -32,11 +32,16 @@ import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
 import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecordVersion;
 import com.liferay.dynamic.data.mapping.model.DDMFormInstanceSettings;
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.model.Value;
 import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
 import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordService;
 import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordVersionLocalService;
 import com.liferay.dynamic.data.mapping.service.DDMFormInstanceService;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceVersionLocalService;
+import com.liferay.dynamic.data.mapping.storage.DDMFormFieldValue;
 import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.mail.kernel.model.MailMessage;
+import com.liferay.mail.kernel.service.MailService;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.Language;
@@ -49,9 +54,14 @@ import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.PrefsProps;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -60,7 +70,9 @@ import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
+import javax.mail.internet.InternetAddress;
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletSession;
@@ -81,6 +93,30 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class AddFormInstanceRecordMVCActionCommand
 	extends BaseMVCActionCommand {
+
+	private static final Map<String, Map<String, String>> messages =
+		HashMapBuilder.<String, Map<String, String>>create(2)
+			.put(
+				"hr",
+				HashMapBuilder.<String, String>create(2)
+					.put(
+						"anonymous-user-email-confirmation-subject",
+						"{0} potvrda")
+					.put(
+						"anonymous-user-email-confirmation-body",
+						"Poštovani,<br/> Vaši podaci su uspješno primljeni. Hvala što ste ispunili obrazac. Možete uređivati svoje podatke <a href='{0}'>ovdje</a>.")
+					.build())
+			.put(
+				"en",
+				HashMapBuilder.<String, String>create(2)
+					.put(
+						"anonymous-user-email-confirmation-subject",
+						"{0} Confirmation")
+					.put(
+						"anonymous-user-email-confirmation-body",
+						"Dear,<br/> your information was successfully received. Thank you for filling out the form. You can edit your data <a href='{0}'>here</a>.")
+					.build())
+			.build();
 
 	@Override
 	protected void doProcessAction(
@@ -238,6 +274,38 @@ public class AddFormInstanceRecordMVCActionCommand
 		}
 	}
 
+	private String _getEmailFromAddress(DDMFormInstance ddmFormInstance)
+		throws Exception {
+
+		DDMFormInstanceSettings formInstancetings =
+			ddmFormInstance.getSettingsModel();
+
+		String defaultEmailFromAddress = _prefsProps.getString(
+			ddmFormInstance.getCompanyId(), PropsKeys.ADMIN_EMAIL_FROM_ADDRESS);
+
+		return GetterUtil.getString(
+			Objects.equals(formInstancetings.emailFromAddress(), "")
+				? null
+				: formInstancetings.emailFromAddress(),
+			defaultEmailFromAddress);
+	}
+
+	private String _getEmailFromName(DDMFormInstance ddmFormInstance)
+		throws Exception {
+
+		DDMFormInstanceSettings formInstancetings =
+			ddmFormInstance.getSettingsModel();
+
+		String defaultEmailFromName = _prefsProps.getString(
+			ddmFormInstance.getCompanyId(), PropsKeys.ADMIN_EMAIL_FROM_NAME);
+
+		return GetterUtil.getString(
+			Objects.equals(formInstancetings.emailFromName(), "")
+				? null
+				: formInstancetings.emailFromName(),
+			defaultEmailFromName);
+	}
+
 	private String _getTimeZoneId(ThemeDisplay themeDisplay) {
 		if (themeDisplay == null) {
 			return StringPool.BLANK;
@@ -246,6 +314,55 @@ public class AddFormInstanceRecordMVCActionCommand
 		User user = themeDisplay.getUser();
 
 		return user.getTimeZoneId();
+	}
+
+	private void _sendConfirmationMail(
+			DDMFormInstance ddmFormInstance,
+			DDMFormInstanceRecord ddmFormInstanceRecord, String emailToAddress,
+			ThemeDisplay themeDisplay)
+		throws Exception {
+
+		MailMessage mailMessage = new MailMessage();
+
+		mailMessage.setFrom(
+			new InternetAddress(
+				_getEmailFromAddress(ddmFormInstance),
+				_getEmailFromName(ddmFormInstance)));
+		mailMessage.setTo(new InternetAddress(emailToAddress));
+
+//		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+//			"content.Language", themeDisplay.getLocale(), getClass());
+//
+//		mailMessage.setSubject(
+//			_language.format(
+//				resourceBundle, "anonymous-user-email-confirmation-subject",
+//				ddmFormInstance.getName(themeDisplay.getLocale()), false));
+//		mailMessage.setBody(
+//			_language.format(
+//				resourceBundle, "anonymous-user-email-confirmation-body",
+//				PortalUtil.getLayoutURL(themeDisplay) + "?uuid=" +
+//				ddmFormInstanceRecord.getUuid(), false));
+
+		Locale locale = themeDisplay.getLocale();
+
+		mailMessage.setSubject(
+				messages
+					.get(locale.getLanguage())
+					.get("anonymous-user-email-confirmation-subject")
+					.replace(
+						"{0}",
+						ddmFormInstance.getName(themeDisplay.getLocale())));
+		mailMessage.setBody(
+			messages
+				.get(locale.getLanguage())
+				.get("anonymous-user-email-confirmation-body")
+				.replace(
+					"{0}",
+					PortalUtil.getLayoutURL(themeDisplay) + "?uuid=" +
+					ddmFormInstanceRecord.getUuid()));
+		mailMessage.setHTMLFormat(true);
+
+		_mailService.sendEmail(mailMessage);
 	}
 
 	private void _updateFormInstanceRecord(
@@ -269,17 +386,63 @@ public class AddFormInstanceRecordMVCActionCommand
 						ddmFormInstance.getVersion(),
 						WorkflowConstants.STATUS_DRAFT);
 
+			DDMFormInstanceRecord ddmFormInstanceRecord;
+
 			if (ddmFormInstanceRecordVersion == null) {
-				_ddmFormInstanceRecordService.addFormInstanceRecord(
-					groupId, ddmFormInstance.getFormInstanceId(), ddmFormValues,
-					serviceContext);
+				ddmFormInstanceRecord =
+					_ddmFormInstanceRecordService.addFormInstanceRecord(
+						groupId, ddmFormInstance.getFormInstanceId(),
+						ddmFormValues, serviceContext);
 			}
 			else {
-				_ddmFormInstanceRecordService.updateFormInstanceRecord(
-					ddmFormInstanceRecordVersion.getFormInstanceRecordId(),
-					false, ddmFormValues, serviceContext);
+				ddmFormInstanceRecord =
+					_ddmFormInstanceRecordService.updateFormInstanceRecord(
+						ddmFormInstanceRecordVersion.getFormInstanceRecordId(),
+						false, ddmFormValues, serviceContext);
+			}
+
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay) actionRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
+			String emailToAddress = _getEmailToAddress(
+				ddmFormValues, themeDisplay);
+
+			if (emailToAddress != null) {
+				_sendConfirmationMail(
+					ddmFormInstance, ddmFormInstanceRecord, emailToAddress,
+					themeDisplay);
 			}
 		}
+	}
+
+	private static String _getEmailToAddress(
+		DDMFormValues ddmFormValues, ThemeDisplay themeDisplay) {
+		DDMFormFieldValue emailDDMFormFieldValue = null;
+
+		for (DDMFormFieldValue ddmFormFieldValue :
+			ddmFormValues.getDDMFormFieldValues()) {
+
+			if (Objects.equals(
+				ddmFormFieldValue.getFieldReference(), "emailAddress")) {
+
+				emailDDMFormFieldValue = ddmFormFieldValue;
+
+				break;
+			}
+		}
+
+		String emailToAddress = null;
+
+		if (emailDDMFormFieldValue != null) {
+			Value value = emailDDMFormFieldValue.getValue();
+
+			Map<Locale, String> localizedValueMap = value.getValues();
+
+			emailToAddress = localizedValueMap.get(themeDisplay.getLocale());
+		}
+
+		return emailToAddress;
 	}
 
 	private void _validateCaptcha(
@@ -337,10 +500,20 @@ public class AddFormInstanceRecordMVCActionCommand
 	private DDMFormInstanceService _ddmFormInstanceService;
 
 	@Reference
+	private DDMFormInstanceVersionLocalService
+		_ddmFormInstanceVersionLocalService;
+
+	@Reference
 	private DDMFormValuesFactory _ddmFormValuesFactory;
 
 	@Reference
 	private Language _language;
+
+	@Reference
+	private MailService _mailService;
+
+	@Reference
+	private PrefsProps _prefsProps;
 
 	@Reference
 	private Portal _portal;
